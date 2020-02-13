377e07e4bf401342c050c31f5082e395df34475d
efrencav <efren@abstract-creative.com>
2020-02-12 22:26:10 -0600

    general terms

--- client/src/components/PDF.js
+++ client/src/components/PDF.js
@@ -157,7 +157,6 @@ function PDF(props) {
                     text: '1.2. The Purchaser has to pay attention especially to the fact that a placed Purchase Order involves his unconditional acceptance of these General Terms of Sale and his rejection of the General Terms of Purchase shown in his commercial documents including especially his order forms and letters.',
                     style: 'small'
                 },
-                ,
                 {
                     text: '2. CONSTITUTION OF THE CONTRACT',
                     style: 'subheader'
@@ -185,7 +184,74 @@ function PDF(props) {
                 {
                     text: '2.6. After being accepted by the FAUREHERMAN Company, no Purchase Order cannot be cancelled barring a prior written agreement by the FAURE HERMAN Company and provided that the whole expenses, resulting from this cancellation, are borne and settled by the Purchaser.',
                     style: 'small'
-                }
+                },
+
+                {
+                    text: '3. CONTENTS OF THE CONTRACT',
+                    style: 'subheader'
+                },
+                {
+                    text: 'FAURE HERMAN bids, quotations, Purchase Order acceptances are strictly limited to supplies and/or services which are expressly mentioned.',
+                    style: 'small'
+                },
+
+                {
+                    text: '4. DOCUMENTATION-ENGINEERING',
+                    style: 'subheader'
+                },
+                {
+                    text: '4.1. The selling price of our goods includes the standard documentation related to each product to be delivered as per the purchase order.',
+                    style: 'small'
+                },
+                {
+                    text: '4.2. Weights, dimensions, capacity, performances and further indications appearing in the documentation detailed above Section 4.1 are approximate indications. These data have a contractual value, only if they are expressly indicated or reminded in the FAURE HERMAN Purchase Order acceptance.',
+                    style: 'small'
+                },
+                {
+                    text: '5. TERMS AND CONDITIONS OF SALE',
+                    style: 'subheader'
+                },
+                {
+                    text: '5.1. Unless expressed at the time of the Purchase Order acceptance, any sale is supposed to be concluded as "Ex-works".',
+                    style: 'small'
+                },
+                {
+                    text: '5.2. The interpretation of trade terms such as "Ex-works" or any other expression, as used in the Contract, shall be in accordance with the International Rules for the Interpretation of Trade Terms (INCOTERMS) released by the International Chamber of Commerce, in force on the date of the formation of the Contract and shall include, for each party, the obligations respectively imposed on them by the aforesaid INCOTERMS.',
+                    style: 'small'
+                },
+                {
+                    text: '5.3. For the FAURE HERMAN Company, the fact of performing, on request and on behalf of the Purchaser, operations other than the ones it falls to him in accordance with the trade terms as defined in the Contract, cannot, in any event, alter either the aforesaid trade terms, or the Contract contents, the Purchaser"s requestto perform such operations, shall involve necessarily the fact that the FAURE HERMAN Company shall act on the Purchaser"s behalf. The aforesaid operations and costs involved will be separately invoiced to the Purchaser, who commits himself to pay the FAURE HERMAN Company on receipt of the invoice. Especially in the case of an "Ex-works" sale, the FAURE HERMAN Company shall not be involved in the haulage Contract of the goods, even though the FAURE HERMAN Company would participate, in whatever forms it maybe, in the loading or stowing operations of the products, so as to ease the Purchaser"s or haulers work.',
+                    style: 'small'
+                },
+                {
+                    text: '6. DELIVERY - PASSING OF TITLE - INSPECTION',
+                    style: 'subheader'
+                },
+                {
+                    text: '6.1. The delivery, the passing of risks of the sold goods are supposed to take place, when these goods are placed at the Purchaser"s disposal, in compliance with the trade terms of the Contract. Transfer of title of the goods shall occur only after the buyer has discharged all its obligation towards FAURE HERMAN and especially the complete payment for the total price of the invoice. Until such time as the products shall have been paid for in full by the purchaser, the purchaser undertakes not to transfer or sell or dispose of the products in any manner whatsoever and shall keep the products so that they can be identified at all times as FAURE HERMAN property.',
+                    style: 'small'
+                },
+                {
+                    text: '6.2. The FAURE HERMAN Company reserves the right to perform partial deliveries with corresponding partial invoicing.',
+                    style: 'small'
+                },
+                {
+                    text: '6.3. All goods are sold and checked by the FAURE HERMAN Company without any inspection by the Purchaser: in case the FAURE HERMAN Purchase Order"s acceptance should schedule an inspection of the goods, this inspection will include, unless expressed otherwise, the following elements:',
+                    style: 'small'
+                },
+                {
+                    text: '- The visual inspection: The Inspector shall check the general aspect of the equipment in compliance with the Purchase Order (paint, cleaning), as well as marking (company"s name plate, tag,...). - The dimensional inspection: Tools (sliding calliper, short measuring tape), as well as drawings shall be made at the disposal of the Inspector for the purpose of the examination. - The performance inspection: by sampling of 1 for 5. The inspection activities will take place within the FAURE HERMAN plant, at the Purchaser"s costs, within s working fortnight, from the date of issue of the written inspection notification and according to the provisions defined in the Contract. Any inspection shall immediately involve the drawing-up of an inspection report signed by both parties.',
+                    style: 'small'
+                },
+                {
+                    text: '6.4. If the Purchaser, who shall be formally advised, does not attend for the purpose of inspection of the good within the time mentioned above, the inspection shall be carried out by the FAURE HERMAN Company, an inspection report will be transmitted to the Purchaser who will not be able to question the accuracy.',
+                    style: 'small'
+                },
+                {
+                    text: '6.5. If the Purchaser or his representative considers that the goods supplied do not comply with the contract specifications, he shall advise the FAURE HERMAN Company by means of a recorded delivery letter with acknowledgement of receipt, which shall be sent within four (4) calendar days at the latest from their reception.',
+                    style: 'small'
+                },
+
             ],
             styles: {
                 header: {
